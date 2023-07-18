c26688f83c6617de8f493fb6d33a149f8a312dae
Got-crypto <pecpalmplay@gmail.com>
2023-07-18 07:58:35 +0200

    added environment variabless

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,12 +3,12 @@ import 'firebase/auth'
 import 'firebase/compat/firestore'
 
 const config = {
-    apiKey: "AIzaSyBx3_fC-Tl75c_6XK9JXuh0lYjRjwVvYC8",
-    authDomain: "food-project-cee2f.firebaseapp.com",
-    projectId: "food-project-cee2f",
-    storageBucket: "food-project-cee2f.appspot.com",
-    messagingSenderId: "837420533344",
-    appId: "1:837420533344:web:df1f62cd6802beaa306505"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECTID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDERID,
+    appId: process.env.FIREBASE_APP_ID
 }
 
 const firebase = Firebase.initializeApp(config)
